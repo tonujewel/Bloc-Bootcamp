f1f83b7f061e9b7dcac1456a980319ae4470153b
jewelrana <tonujewel@gmail.com>
2022-06-22 13:09:26 +0600

    cubit example

---
+++ android/app/src/main/kotlin/com/tonujewel/bloc/bootcamp/bloc_bootcamp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tonujewel.bloc.bootcamp.bloc_bootcamp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'dart:math' as math show Random;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+const name = [
+  'Foo',
+  'Bar',
+  'Baz',
+];
+
+extension RandomElement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
+class NameCubit extends Cubit<String?> {
+  NameCubit() : super(null);
+
+  void pickRandomName() => emit(name.getRandomElement());
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final NameCubit cubit;
+
+  @override
+  void initState() {
+    super.initState();
+    cubit = NameCubit();
+  }
+
+  @override
+  void dispose() {
+    cubit.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Bloc Bootcamp")),
+      body: StreamBuilder<String?>(
+        stream: cubit.stream,
+        builder: (context, snapshot) {
+          final button = TextButton(
+            onPressed: () => cubit.pickRandomName(),
+            child: const Text(
+              'Pick a random number',
+            ),
+          );
+
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return button;
+
+            case ConnectionState.waiting:
+              return button;
+
+            case ConnectionState.active:
+              return Column(
+                children: [
+                  Text(snapshot.data ?? ''),
+                  button,
+                ],
+              );
+
+            case ConnectionState.done:
+              return const SizedBox();
+          }
+        },
+      ),
+    );
+  }
+}
